07d2ef12f23ce677b68deb5814caff04de98ee95
Sangeeta-g <sangeetadani599@gmail.com>
2025-10-25 13:17:44 +0530

    import state

--- feeds/views.py
+++ feeds/views.py
@@ -6,6 +6,8 @@ from .models import *
 from rest_framework import generics
 from .serializers import *
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.exceptions import NotFound
+
 
 class CreateFeedAPIView(APIView):
     permission_classes = [IsAuthenticated]
