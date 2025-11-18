c73e3a7c901538e2811afef00531f538c193c30e
Sangeeta-g <sangeetadani599@gmail.com>
2025-11-18 10:22:53 +0530

    moved cors

--- chatapp_backend/settings.py
+++ chatapp_backend/settings.py
@@ -80,6 +80,7 @@ if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -87,7 +88,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+    
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
